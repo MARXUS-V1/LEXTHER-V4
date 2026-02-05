90e5096d4f46f362ab76db72bfd37e0c29285a7b
MARXUS-V1 <vishnudevan420@gmail.com>
2026-02-05 21:17:47 +0530

    Tracker.py
    na

---
+++ tracker.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+from datetime import datetime
+
+#List of stocks to track
+tickers = ["AAPL", "TSLA", "BTC-USD"]
+
+print(f"market update:
+{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+                         
+print("-"*30)
+
+for ticker in tickers:
+  data=yf.Ticker(ticker)
+  price=data.history(periods=1d")
+  ['Close'].iloc[-1]
+  print(f"{ticker}:${price:.2f}")
